f79c653cc4ab4f3d562f4ac156ab60ec57c25fa9
Vitor (Vash) <vashgithub@gmail.com>
2026-05-22 01:01:34 -0300

    sintax of arrays and functions

--- basic/public/array.php
+++ basic/public/array.php
@@ -1,2 +1,28 @@
 <?php
 // Arrays
+$data = ["Penis", 34, 34.6, true, "azul", false];
+
+$data[6] = "teste";
+
+$length = count($data);
+$data[$length] = "last";
+
+$length = count($data);
+$data[$length] = "peste";
+
+array_push($data, "qualquer"); // ao final do array
+array_unshift($data, 1); // no começo do array
+
+var_dump($data);
+var_dump($data[3]);
+//print_r($data);
+echo "-------------------\n";
+
+$indices = [
+    "comida" => "maçã",
+    "cor" => "vermelho",
+    "validade" => 30,
+    "lista" => ["higiene" => "papel higienico", "açougue" => "carne"],
+];
+
+var_dump($indices["lista"]["higiene"]);
---
+++ basic/public/convertion.php
@@ -0,0 +1,4 @@
+<?php
+// Conversão de dados
+// string, boolean, float, integer, double, object, array,
+// intVal, floatVal, strVal, boolVal
