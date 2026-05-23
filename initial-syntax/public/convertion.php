f685c6e0163e5581792717e2fbc6fc55b8021014
Vitor (Vash) <vashgithub@gmail.com>
2026-05-22 21:31:40 -0300

    loops: for, while, do while

--- initial-syntax/public/convertion.php
+++ initial-syntax/public/convertion.php
@@ -1,4 +1,25 @@
 <?php
 // Conversão de dados
-// string, boolean, float, integer, double, object, array,
+// (string), (boolean), (float), (integer), (double), (object), (array),
 // intVal, floatVal, strVal, boolVal
+
+$name = "Nome qualquer";
+$arrayTest = [
+    "nome" => ["Vitor", "Francisco"],
+    "idade" => [34, 35],
+    "numero" => 10,
+];
+
+$change = (object) $arrayTest;
+
+var_dump($name);
+
+var_dump($change);
+var_dump($change->nome[0]);
+
+// Outro forma
+$value = 399;
+
+$resource = boolval($value);
+
+var_dump($resource);
---
+++ initial-syntax/public/loopings.php
@@ -0,0 +1,38 @@
+<?php
+echo "----------------For------------------";
+echo "\n";
+// Loop For
+$values = ["Ferdinando", "Jose", "Maria", "Gabriel"];
+
+for ($i = 0; $i < count($values); $i++) {
+    echo $values[$i] . " ";
+}
+echo "\n";
+echo "\n";
+
+echo "----------------While------------------";
+echo "\n";
+// Loop While : testa a condição antes de executar o bloco, podendo nem sequer rodar.
+$j = 0;
+echo "Pré incremento: " . $j . "\n";
+while ($j < count($values)) {
+    echo $values[$j] . " ";
+    $j++;
+}
+echo "\nPós incremento: " . $j;
+echo "\n";
+echo "\n";
+//Independente do bloco da variável, ela incrementa de todo jeito
+echo "----------------Do While------------------";
+echo "\n";
+// Do While - Faça Enquanto :   Testa a condição depois, garantindo que rode pelo menos uma vez
+$k = 0;
+do {
+    echo $values[$k] . " ";
+    $k++;
+} while ($k < count($values));
+echo "\n";
+echo "\n";
+
+echo "----------------Foreach------------------";
+echo "\n";
