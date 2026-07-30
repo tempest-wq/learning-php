7a08001249a20630e282e02a4299793815807356
Vitor (Vash) <vashgithub@gmail.com>
2026-07-30 02:17:36 -0300

    parent for static e property methods

--- complete-oop/public/index.php
+++ complete-oop/public/index.php
@@ -2,3 +2,37 @@
 declare(strict_types=1);
 require '../vendor/autoload.php';
 
+//parent SEMPRE será utilizado na classe filha para se referenciar a classe pai
+//parent so funciona para propriedades estáticas, métodos podem ser estático ou n
+class Person
+{
+    public function __construct()
+    {
+        var_dump('Classe pai');
+    }
+    public static function info()
+    {
+        return 'person info classe pai';
+    }
+    public function data()
+    {
+        return 'Dados da classe pai';
+    }
+}
+class User extends Person
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function parentTest()
+    {
+        return parent::info();
+    }
+    public function data()
+    {
+        return parent::data();
+    }
+}
+
+echo (new User)->data();
